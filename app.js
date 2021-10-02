f76003edbc675b3b3a94d6e296518c1e644d85be
Serajj <39436512+Serajj@users.noreply.github.com>
2021-10-02 19:57:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,88 +2,24 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-const NodeMediaServer = require('node-media-server');
 let portserver = 8000;
 
 
-
-
+const NodeMediaServer = require('node-media-server');
 
 const config = {
-    rtmp: {
-        port: 1935,
-        chunk_size: 60000,
-        gop_cache: true,
-        ping: 30,
-        ping_timeout: 60
-    },
-    http: {
-        port: portserver,
-        allow_origin: '*'
-    },
-    trans: {
-        ffmpeg: ffmpegPath,
-        tasks: [
-            {
-                app: 'live',
-                ac: 'aac',
-            }
-        ]
-    }
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
 };
 
 var nms = new NodeMediaServer(config)
-
-try {
-    nms.run();
-    console.log("Initiated Media Server");
-} catch (error) {
-
-}
-
-
-nms.on('preConnect', (id, args) => {
-    console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-});
-
-nms.on('postConnect', (id, args) => {
-    console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-nms.on('doneConnect', (id, args) => {
-
-});
-
-nms.on('prePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    // // let session = nms.getSession(id);
-    // // session.reject();
-});
-
-nms.on('postPublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('donePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('prePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-});
-
-nms.on('postPlay', (id, StreamPath, args) => {
-    var datetime = dateFormat(new Date(), "yyyy-mm-dd-HH-MM-ss");
-    console.log('[connected time : ] ' + datetime);
-    console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    // var cod = savestreamtodb(id, StreamPath, datetime);
-    // console.log(cod)
-});
-
-nms.on('donePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
+nms.run();
